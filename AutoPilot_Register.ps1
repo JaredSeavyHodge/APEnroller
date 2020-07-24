9f3dbca9f66edf5a2d454962b498f82818c9ecf2
JaredSeavyHodge <jaredseavy@gmail.com>
2020-07-23 22:57:55 -0400

    Update AutoPilot_Register.ps1

--- AutoPilot_Register.ps1
+++ AutoPilot_Register.ps1
@@ -5,6 +5,11 @@ Test-WindowsEditionforAutopilot                             #Will Upgrade Home/C
 $Serial = Get-DeviceSerial                                  #Creates CimSession and Returns Device serial
 Connect-Tennant                                             #Connects to Azure and MS-Graph - Imports Dependency Modules if Necessary
 Test-AutopilotForExistingDevice -DeviceToCheck $Serial      #Check for Device Existence in Autopilot to Avoid Bug Behavior
+Test-CheckForUnattendXML                                    #Will Remove any answerfiles in the Sysprep or Panther folder
+
+if ((Read-Host "Do you want to remove any unattend files from this device? These answer files can sometimes cause issues. (Y/N) Default=N") -eq "Y"){
+    Test-CheckForUnattendXML                                #Will check for and Remove any answerfiles in the Sysprep or Panther folders.
+}
 
 #Out-Grid for User to Select Azure Group to Add This Device Too
 $Group = Get-AzureADGroup -SearchString "ENDPOINT Devices" | Out-GridView -OutputMode Single
