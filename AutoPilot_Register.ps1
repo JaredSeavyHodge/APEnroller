99b767adcbc7fd444c7334e1804b946353c81845
JaredSeavyHodge <jaredseavy@gmail.com>
2020-07-23 21:55:06 -0400

    Update AutoPilot_Register.ps1

--- AutoPilot_Register.ps1
+++ AutoPilot_Register.ps1
@@ -1,20 +1,18 @@
 #. .\Functions-AutopilotValidation.ps1
 iex(iwr https://raw.githubusercontent.com/JaredSeavyHodge/APEnroller/master/Functions-AutopilotValidation.ps1)
 
-Test-WindowsEditionforAutopilot
-$Serial = Get-DeviceSerial
-Connect-Tennant
-Test-AutopilotForExistingDevice -DeviceToCheck $Serial
+Test-WindowsEditionforAutopilot                             #Will Upgrade Home/Core Edition to Education to Support Autopilot
+$Serial = Get-DeviceSerial                                  #Creates CimSession and Returns Device serial
+Connect-Tennant                                             #Connects to Azure and MS-Graph - Imports Dependency Modules if Necessary
+Test-AutopilotForExistingDevice -DeviceToCheck $Serial      #Check for Device Existence in Autopilot to Avoid Bug Behavior
 
+#Out-Grid for User to Select Azure Group to Add This Device Too
 $Group = Get-AzureADGroup -SearchString "ENDPOINT Devices" | Out-GridView -OutputMode Single
-
-$confirmation = Read-Host "Next, the device info will be uploaded, registered with Windows AutoPilot and enrolled in AAD/MDM.  Continue? (Y/N) Default: Y"
+$confirmation = Read-Host "The device info will be uploaded, registered with Windows AutoPilot, and enrolled in AAD/MDM.  Continue? (Y/N) Default: Y"
 if ($confirmation -eq 'n') {
     exit
 }
 
-<#
 Install-Script -Name Get-WindowsAutoPilotInfo -Force
 Get-WindowsAutoPilotInfo.ps1 -Online -Assign -AddToGroup ($Group.DisplayName) -reboot
-Write-Host "Script has finished"
-#>
+Write-Host "Rebooting?"
