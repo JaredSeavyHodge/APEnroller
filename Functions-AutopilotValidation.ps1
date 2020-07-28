5a07a0ae83a7550d4de83f30bd3a495d17d45834
JaredSeavyHodge <jaredseavy@gmail.com>
2020-07-28 09:59:55 -0400

    Update Functions-AutopilotValidation.ps1

--- Functions-AutopilotValidation.ps1
+++ Functions-AutopilotValidation.ps1
@@ -82,7 +82,7 @@ Function Test-CheckForUnattendXML {
                 $Sysprep = $true
             }else{Write-Host "Unattend file at $i not found"}
         }
-        if ($Sysprep){sysprep.exe /generalize /oobe /reboot}
+        if ($Sysprep){$env:systemroot\system32\sysprep\sysprep.exe /generalize /oobe /reboot}
     }        
 }
 
